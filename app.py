a2a28bed3b1bfaeb5a3d56797bc17b3b4bdeb222
shiwusa <shiwusa@ubuntu.myguest.virtualbox.org>
2023-01-05 22:44:50 +0100

    add jwt secret key

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_smorest import Api
 
 from db import db
@@ -18,9 +21,12 @@ def create_app():
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     db.init_app(app)
 
     api = Api(app)
+
+    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()
